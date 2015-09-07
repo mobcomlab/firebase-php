7ac4d404220dd1a8d33dbf0348797a145d9f5560
Michael Dimmock <michael.dimmock@virgin.net>
2015-09-07 10:11:29 +0100

    psr7 changes

--- src/Normalizer/SimpleArrayNormalizer.php
+++ src/Normalizer/SimpleArrayNormalizer.php
@@ -1,7 +1,7 @@
 <?php namespace Firebase\Normalizer;
 
 
-use GuzzleHttp\Message\ResponseInterface;
+use Psr\Http\Message\ResponseInterface;
 
 class SimpleArrayNormalizer extends AbstractNormalizer implements NormalizerInterface {
 
